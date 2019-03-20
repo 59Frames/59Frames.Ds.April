e0771bcb3689fae95a9af19a59263e5151112731
danielseifert <info@danielseifert.ch>
2019-03-20 14:41:56 +0100

    Added LWJGL and fixed JNA Dependency
    implemented concurrency
    
    Added Arc information
    
    Implemented ThreadService

--- src/April.java
+++ src/April.java
@@ -1,8 +1,18 @@
-import module.knowledge.crawler.WebSpider;
-import util.StringUtil;
+import util.CollectionUtil;
+import util.RandomUtil;
+
+import java.util.ArrayList;
 
 public class April {
     public static void main(String[] args) {
+        ArrayList<Integer> newList = new ArrayList<>();
 
+        for (int i = 0; i < 100; i++) {
+            newList.add(RandomUtil.random(0, 50));
+        }
+
+        var testList = CollectionUtil.clearAndRemoveIf(newList, val -> val.equals(0));
+
+        System.out.println(testList.size());
     }
 }
