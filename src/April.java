80be52bf9dbcc7a7942ae9035255b59bb3beda97
Daniel Seifert <info@danielseifert.ch>
2019-03-22 17:38:01 +0100

    added lando

--- src/April.java
+++ src/April.java
@@ -1,5 +1,9 @@
+import _59frames.ds.lando.CommandListener;
+
 public class April {
     public static void main(String[] args) {
+        var listener = CommandListener.builder().build();
 
+        listener.start();
     }
 }
