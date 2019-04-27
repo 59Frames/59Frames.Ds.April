799220b3f302c1aec37ccd74284f8d4ea2da7056
Daniel Seifert <info@danielseifert.ch>
2019-04-27 21:17:07 +0200

    updated toolbox

--- src/April.java
+++ src/April.java
@@ -1,16 +1,21 @@
-import model.interfaceable.Bootable;
 import module.bootstrap.Bootstrap;
-import module.speech.language.SpellCorrector;
-import util.CollectionUtil;
-
-import java.lang.reflect.Method;
-import java.util.ArrayList;
 
 import static util.Toolbox.*;
 
 public class April {
 
     public static void main(String[] args) throws Exception {
+
+        double[] arr = new double[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random();
+        }
+
+        System.out.println(average(arr));
+
+        System.out.println(mph2kmh(180));
+
         new Bootstrap().boot();
     }
 }
